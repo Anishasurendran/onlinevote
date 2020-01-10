fa1c122b793bbd53d37a26ceaebb245e3ff81c31
Anu <sinianu2019@gmail.com>
2020-01-10 10:17:49 +0530

    loginsucc

---
+++ app/src/main/java/com/example/onlinevote/Capture.java
@@ -0,0 +1,14 @@
+package com.example.onlinevote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Capture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.voterlist);
+    }
+}
--- app/src/main/java/com/example/onlinevote/MainActivity.java
+++ app/src/main/java/com/example/onlinevote/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.onlinevote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText enterAdharnumber;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+        enterAdharnumber=(EditText)findViewById(R.id.adharnum);
+        login=(Button)findViewById(R.id.logsubmit);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
