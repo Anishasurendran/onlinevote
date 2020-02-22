8c43c8e6fe8b3e74603ab2e6ccbf5e91b1f03ddf
Anu <sinianu2019@gmail.com>
2020-02-22 11:01:04 +0530

    ok

--- app/src/main/java/com/example/onlinevote/retrofit/AppClient.java
+++ app/src/main/java/com/example/onlinevote/retrofit/AppClient.java
@@ -20,7 +20,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AppClient {
 
-    public static final String MASTEERURL = "http://192.168.137.183:8000/";
+    public static final String MASTEERURL = "http://192.168.43.221:8000/";
 
     public static Retrofit retrofit = null;
 
